1c92f4148adcd3f2749924a085b816ca70edd8c2
Louis <louis@hamelers.nl>
2020-03-21 22:02:35 +0100

    register done apart from google and facebook login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD5Tykp7GjIZp-W8lwT39cYd85QXRvXUEw',
+    authDomain: 'duelr-9306d.firebaseapp.com',
+    databaseURL: 'https://duelr-9306d.firebaseio.com',
+    projectId: 'duelr-9306d',
+    storageBucket: 'duelr-9306d.appspot.com',
+    messagingSenderId: '22826497018',
+    appId: '1:22826497018:web:5e22f59c2d817c78a66a43',
+    measurementId: 'G-3NHGWPFYNS'
+  }
 };
