be16b21a486f246e9977530061959264ef0d0e83
Louis <louis@hamelers.nl>
2020-04-04 14:40:14 +0200

    removed staging build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyDb5WgJfJplWAO7W5zKErfJ-rADoNNvbgQ',
-    authDomain: 'duelr-testenv.firebaseapp.com',
-    databaseURL: 'https://duelr-testenv.firebaseio.com',
-    projectId: 'duelr-testenv',
-    storageBucket: 'duelr-testenv.appspot.com',
-    messagingSenderId: '434326310539',
-    appId: '1:434326310539:web:71f9fb2d4a5087bd720e8e',
-    measurementId: 'G-0Y7FNYGW9R'
+    apiKey: 'AIzaSyD5Tykp7GjIZp-W8lwT39cYd85QXRvXUEw',
+    authDomain: 'duelr-9306d.firebaseapp.com',
+    databaseURL: 'https://duelr-9306d.firebaseio.com',
+    projectId: 'duelr-9306d',
+    storageBucket: 'duelr-9306d.appspot.com',
+    messagingSenderId: '22826497018',
+    appId: '1:22826497018:web:5e22f59c2d817c78a66a43',
+    measurementId: 'G-3NHGWPFYNS'
   }
 };
